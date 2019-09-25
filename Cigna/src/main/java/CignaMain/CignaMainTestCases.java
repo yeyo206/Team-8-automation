fbbe89e4546a22db3ec459316d53b1220497dacc
mac <mac@Macs-MBP-5.attlocal.net>
2019-09-24 21:06:24 -0400

    top links list

--- Cigna/src/main/java/CignaMain/CignaMainTestCases.java
+++ Cigna/src/main/java/CignaMain/CignaMainTestCases.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.List;
+
 public class CignaMainTestCases extends CommonAPI {
 
 
@@ -18,7 +20,7 @@ public class CignaMainTestCases extends CommonAPI {
     @FindBy(xpath = "//a[contains(text() , 'Privacy')]") public static WebElement privacyButton;
     @FindBy(xpath = "//h1") public static WebElement header;
     @FindBy(xpath = "//ul[@class = 'navbar-nav align-items-center ml-auto universal-navigation-buttons']/li/a")
-    public static WebElement topLinks;
+    public static List<WebElement> topLinks;
     public void clickPrivacyButton(){
         privacyButton.click();
         Assert.assertTrue(header.getText().toLowerCase().contains("privacy"));
